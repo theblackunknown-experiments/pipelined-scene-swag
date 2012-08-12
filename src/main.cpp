e508cba86e12d76838ac12756cb8b7a39d23b2e2
MACHIZAUD Andréa <andrea.machizaud@gmail.com>
2012-08-12 21:45:18 +0200

    bugfix: misused glut elapsed time

--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,12 @@ void idle_callback( )
         //which can result into heating the system
         static std::size_t function_call_counter = 0u;
         static std::size_t accumulated_time      = 0u;
+        static std::size_t previous_tick         = 0u;
 
-        const auto elapsed_time = glutGet(GLUT_ELAPSED_TIME);
+        const auto current_tick = glutGet(GLUT_ELAPSED_TIME);
 
-        accumulated_time += elapsed_time;
+        accumulated_time += (current_tick - previous_tick);
+        previous_tick = current_tick;
         
         //We need to reset the time accumulator, in order to enable function calls for the next time frame
         if( accumulated_time > TimeFrame )
@@ -76,9 +78,10 @@ int main(int argc, char* argv[])
     {
         glutInit(&argc, argv);
         //Context
-        glutInitContextVersion( 3, 3 );
-        glutInitContextProfile( GLUT_CORE_PROFILE );
-        glutInitContextFlags  ( GLUT_FORWARD_COMPATIBLE | GLUT_DEBUG );
+        //ERROR: Doesn't work on my linux, using open-source driver...
+        //glutInitContextVersion( 3, 3 );
+        //glutInitContextProfile( GLUT_CORE_PROFILE );
+        //glutInitContextFlags  ( GLUT_FORWARD_COMPATIBLE | GLUT_DEBUG );
         //Window
         const auto screen_width = glutGet( GLUT_SCREEN_WIDTH );
         const auto screen_height = glutGet( GLUT_SCREEN_HEIGHT );
@@ -116,6 +119,51 @@ int main(int argc, char* argv[])
 
         std::cout << "Freeglut version: " << vmajor << "." << vminor << "-" << vpatch << std::endl;  
         std::cout << "GLEW version    : " << glew_version << std::endl;
+
+        const std::function<GLboolean(void)> OpenGLVersionQuery[] = {
+        		[] ( ) -> GLboolean { return GLEW_VERSION_4_1; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_4_0; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_3_3; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_3_2; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_3_1; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_3_0; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_2_1; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_2_0; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_1_5; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_1_4; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_1_3; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_1_2_1; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_1_2; },
+        		[] ( ) -> GLboolean { return GLEW_VERSION_1_1; },
+        };
+
+		std::cout << "OpenGL API compatible: " << std::endl;
+        const std::size_t openGLVersionCount   = sizeof(OpenGLVersionQuery) / sizeof(OpenGLVersionQuery[0]);
+        for(std::size_t openGLVersionChecker = 0; openGLVersionChecker < openGLVersionCount; ++openGLVersionChecker)
+        {
+        	if( const GLboolean version_identifier = OpenGLVersionQuery[openGLVersionChecker]( ) )
+        	{
+        		std::string version_title;
+        		switch(openGLVersionChecker)
+        		{
+        		case  0: version_title = "4.1"; break;
+        		case  1: version_title = "4.0"; break;
+        		case  2: version_title = "3.3"; break;
+        		case  3: version_title = "3.2"; break;
+        		case  4: version_title = "3.1"; break;
+        		case  5: version_title = "3.0"; break;
+        		case  6: version_title = "2.1"; break;
+        		case  7: version_title = "2.0"; break;
+        		case  8: version_title = "1.5"; break;
+        		case  9: version_title = "1.4"; break;
+        		case 10: version_title = "1.3"; break;
+        		case 11: version_title = "1.2.1"; break;
+        		case 12: version_title = "1.2"; break;
+        		case 13: version_title = "1.1"; break;
+        		}
+        		std::cout << "\t-" << version_title << std::endl;
+        	}
+        }
     }
 
     //Setup global application 
@@ -126,6 +174,8 @@ int main(int argc, char* argv[])
     glutDisplayFunc ( display_callback );
     glutReshapeFunc ( reshape_callback );
 
+    //Reset timer just before main loop (ignore elapsed time during initialization)
+    glutGet(GLUT_ELAPSED_TIME);
     glutMainLoop( );
 
     return 0;
